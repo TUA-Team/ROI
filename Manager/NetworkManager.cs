00ffda4ba6239ce5b9a0ac07fbee0614009f3a67
Dradonhunter11 <dradonhunter11@gmail.com>
2019-05-11 10:03:16 -0400

    Initial TA 7 commit, now ROI!

---
+++ Manager/NetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using ROI.Manager;
+using System;
+using System.IO;
+using ROI.Enums;
+using ROI.Players;
+using Terraria;
+
+namespace ROI.Network
+{
+    internal class NetworkManager : BaseInstanceManager<NetworkManager>
+    {
+
+        public override void Initialize()
+        {
+            
+        }
+
+        public void ReceivePacket(BinaryReader networkReader, int whoAmI)
+        {
+            ROINetworkMessage networkMessage = (ROINetworkMessage) networkReader.ReadByte();
+            switch (networkMessage)
+            {
+                case ROINetworkMessage.PlayerData:
+                    Main.player[whoAmI].GetModPlayer<ROIPlayer>().ReceiveNetworkData(networkReader);
+                    break;
+            }
+        }
+    }
+}
---
+++ Manager/VoidManager.cs
@@ -0,0 +1,73 @@
+﻿using ROI.Players;
+
+namespace ROI.Manager
+{
+    internal sealed class VoidManager : BaseInstanceManager<VoidManager>
+    {
+        public override void Initialize()
+        {
+
+        }
+
+        public float Percent(ROIPlayer player)
+        {
+            return player.VoidAffinityAmount * 100f / player.MaxVoidAffinity;
+        }
+
+        /// <summary>
+        /// Unlock the player tier locally, then will send to the server, it's a global unlock anyway for everyone in the server
+        /// </summary>
+        public void UnlockTier(ROIPlayer player, int tier)
+        {
+            if (player.VoidTier == tier - 1)
+                player.VoidTier = tier;
+        }
+
+        #region Void Effect
+        public void Effect(ROIPlayer target)
+        {
+
+        }
+
+        private bool Tier1Effect(ROIPlayer target)
+        {
+            return false;
+        }
+
+        private bool Tier2Effect(ROIPlayer target)
+        {
+            return false;
+        }
+
+        private bool Tier3Effect(ROIPlayer target)
+        {
+            return false;
+        }
+
+        private bool Tier4Effect(ROIPlayer target)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Possible effect can range 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private bool Tier5Effect(ROIPlayer target)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Void pillar boss fight, will drain all your void affinity if it spawn
+        /// </summary>
+        /// <param name="target"></param>
+        private void Tier6Effect(ROIPlayer target)
+        {
+
+        }
+        #endregion
+
+    }
+}
